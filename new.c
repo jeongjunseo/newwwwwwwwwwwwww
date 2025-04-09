b9b68b5bcb4888240e26de5f0f6a249ff3255053
jeongjunseo <jjs235744@gmail.com>
2025-04-09 18:23:56 +0900

    updated code

---
+++ new.c
@@ -0,0 +1,24 @@
+package nestnet;
+import java.util.Scanner;
+public class Apr1 {
+	public static void main(String[] args) {
+:		int N, M;
+		Scanner scanner=new Scanner(System.in);
+		N=scanner.nextInt();
+		M=scanner.nextInt();
+		String[] name=new String[N+M];
+		Stirng[] name=
+		for(int i=0;i<N+M;i++) {
+			name[i]=scanner.nextLine();
+		}
+		for(int i=0;i<N;i++) {
+			for(int j=M;j<M+N;j++) {
+				if(name[i].equals(name[M])) {
+					
+				}
+			}
+		}
+		
+	}
+}
+
